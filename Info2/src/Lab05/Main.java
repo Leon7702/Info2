692fe868b92b10278f665f90b903749938416068
Leon7702 <leon@192.168.57.32>
2023-05-15 19:06:49 +0200

    struggling with the flag lol

--- Info2/src/Lab05/Main.java
+++ Info2/src/Lab05/Main.java
@@ -3,8 +3,35 @@ package Lab05;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String outputLine;  
+		for (int row = 1; row <= 40; row++){      
+		    outputLine = "";      
+		    for (int column = 1; column <= 40; column++){
+		        outputLine = outputLine + determineCharacter(column, row);
+		    }      
+		    System.out.println (outputLine);
+		} 
+	}
+
+	public static char determineCharacter (int col, int row) {
+		//if statements for '?'
+		if (row == 1 || row == 2 || row == 39 || row == 40) return '?';
+		if (col == 1 || col == 2 || col == 39 || col == 40) return '?';
+		//if statements for '/'
+		if (row >= 3 && row <= 17 && col >=3 && col <= 16) {
+			if ((row % 2 != 0) && (col % 3 == 0)) return '/';
+			if ((row % 2 == 0) && (col % 3 == 2)) return '/';
+
+			if ((row % 2 == 0) && (col % 3 == 1)) return '=';
+			if ((row % 2 == 0) && (col % 3 == 2)) return ' ';
+
+
+		}
+
+	
+		return ' ';
+		
 	}
 
 }
